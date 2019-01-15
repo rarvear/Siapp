e5fdb8268e82f046ef0e27acca1097c0566af245
rarvear <rarvear@gmail.com>
2019-01-15 17:14:09 -0300

    Landing Page

---
+++ config/initializers/assets.rb
@@ -0,0 +1,23 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css', 'magnific-popup')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css', 'owl-carousel')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js', 'particles')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'video')
+
+Rails.application.config.assets.precompile += %w( *.jpg *.png *.m4v *.mp4 *.webm *.ogv)
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
